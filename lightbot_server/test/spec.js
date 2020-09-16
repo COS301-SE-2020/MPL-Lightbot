a26f8db4408ed1939de146a1ab262532fc06314f
LauraLeeStrydom <u04874359@tuks.co.za>
2020-09-16 12:33:41 +0200

    Implementing testing files

---
+++ lightbot_server/test/spec.js
@@ -0,0 +1,28 @@
+describe('Sample Test', () => {
+    it('should test that true === true', () => {
+      expect(true).toBe(true)
+    })
+  })
+// const RegisterView = require("../../lightbot_web/src/views/Register");
+
+// describe("The User class", () => {
+//   it("should create a new user", () => {
+//     const user1 = new RegisterView("sam", "smith", "smith@test.com", "12345678", "12345678");
+
+//     expect(user1).toEqual({
+//       name: "sam",
+//       surname: "smith",
+//       email: "smith@test.com",
+//       password: "12345678",
+//       confirmpass: "12345678"
+//     });
+//   });
+  // describe("The validateName function", () => {
+  //   it("should create new error message if the user name is less than 5 characters", () => {
+  //     const user2 = new RegisterView("tom", "tom@test.com");
+  //     user2.validateName();
+
+  //     expect(user2.errors).toEqual(["the name must be at least 5 chars long"]);
+  //   });
+  // });
+//});
